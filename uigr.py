4a91685e376bab3e7f1560a9b00f2ab3a7e01681
James Harris <james.harris.1@gmail.com>
2015-10-03 14:48:22 +0100

    First set of UI tests working

--- uigr.py
+++ uigr.py
@@ -42,140 +42,171 @@ else:
   from tkinter import ttk
 
 
+#******************************************************************************
+#
+# App class
+#
+#******************************************************************************
+
 class App(object):
   'An application. A collector for active windows'
 
   def __init__(self):
+    self._root = None
+    self._windows = []
+
+  def window_add(self, window):
+    if window not in self._windows:
+      self._windows.append(window)
+    else:
+      raise ValueError, 'App already has window %s' % window
+
+  def window_remove(self, window):
+    try:
+      self._windows.remove(window)
+    except ValueError:
+      raise ValueError, 'App does not have window %s' % window
+    if len(self._windows) == 0:
+      self._root.destroy()
+
+  def implement(self):
+    'Implement this app in the chosen UI toolkit'
     self._root = tk.Tk()
     self._root.wm_title('Default window')
-    self._root.withdraw()
-    self._children = []
-    self._nchildren = 0 # Ignoring the default (withdrawn) window
+    self._root.withdraw()  # Don't use the default window
+    for window in self._windows:
+      window.implement(self)
 
-  def act(self):
+  def interact(self):
     'Process the windows and widgets and their callbacks'
-    for child in self._children:
-      child.implement()
+    if not self._root:
+      self.implement()
     self._root.mainloop()
+    # App has closed. Nothing to pass back to caller
 
-  def child_new(self, child):
-    'Add a new child'
-    if child not in self._children:
-      self._children.append(child)
-      self._nchildren += 1
-    else:
-      raise KeyError, 'Child %s already present' % child
 
-  def child_killed(self, child):
-    'Delete an existing child'
-    for i in range(self._nchildren):
-      if self._children[i] == child:
-        del self._children[i]
-        self._nchildren -= 1
-        if self._nchildren == 0:
-          self._root.destroy()
-        break
-    else:
-      raise KeyError, 'Child %s not present' % child
+#******************************************************************************
+#
+# Window class
+#
+#******************************************************************************
+
+class Window(object):
+  def __init__(self, title = ''):
+    self._title = title
+    self._menus = []
+    self._impl = None  # Implementation
+    self._parent = None
+
+  def destroy(self):
+    if self._impl:
+      self._impl.destroy()
+    if self._parent:
+      self._parent.window_remove(self)
+
+  def menu_add(self, menu):
+    self._menus.append(menu)
+
+  def implement(self, parent):
+    'Implement the elements of the window'
+    self._parent = parent
+    self._impl = tk.Toplevel()
+    self._impl.wm_title(self._title)
+    self._impl.protocol(name = 'WM_DELETE_WINDOW', func = self.destroy)
+
+    if len(self._menus) > 1:
+      raise ValueError, 'Have %s menus instead or 0 or 1.' % len(self._menus)
+
+    if len(self._menus) == 1:
+#      print ' _menus[0] =', self._menus[0]
+      menubar = self._menus[0].make()  # Just the first menu
+      self._impl.config(menu = menubar)
+
+
+#******************************************************************************
+#
+# Action class
+#
+#******************************************************************************
 
 class Action(object):
   'An action that a UI element can cause'
 
-  def __init__(self, label = '', desc = '', action = None, icon = None):
+  def __init__(self, label, selector_offset,
+       desc = '', action = None, parms = None, icon = None):
     self.label  = label
+    self.seloff = selector_offset
     self.desc   = desc
     self.action = action
+    self.parms  = parms
     self.icon   = icon
 
-class Master_window(object):
-  def __init__(self, parent = None, title = ''):
-    ptype = type(parent)
-    if ptype != App:
-      raise ValueError, \
-         'Parent of a master window must be an app, not %s' % ptype
-    parent.child_new(self)
-    self._parent = parent
-    self._title = title
-    self.menus = []
 
-  def killed(self):
-    self._real.destroy()
-    self._parent.child_killed(self)
-
-  def implement(self):
-    'Implement the elements of the window'
-    self._real = tk.Toplevel()
-    self._real.wm_title(self._title)
-    self._real.protocol(name = 'WM_DELETE_WINDOW', func = self.killed)
-
-    # Implement the menu, if present
-    if self.menus:
-      for m in self.menus:
-        print ' menu:', m
-        for v in m:
-          print ' ', v
-
-#      menubar = tk.Menu(self)
-
-
-class Slave_window(object):
-  def __init__(self, parent = None, title = ''):
-    ptype = type(parent)
-    if ptype == Master_window or ptype == Slave_window:
-      self._real = tk.Toplevel()
-      self._real.wm_title(title)
-      parent.child_new(self)
-      self._real.protocol(name = 'WM_DELETE_WINDOW', func = self.killed)
-
-  def killed(self):
-    self._real.destroy()
-    self._parent.child_killed(self)
-
-
-class Element(object): pass
-
+#******************************************************************************
+#
+# Menu class
+#
+#******************************************************************************
 
 class Menu(object):
-  def __init__(self, value = []):
-    self._value = value
+  def __init__(self, label, selector_offset, desc=''):
+    self.label    = label
+    self._seloff   = selector_offset
+    self.desc = desc
+    self._entries  = []
+
+  def action_add(self, action):
+    self._entries.append(('a', action))
+
+  def menu_add(self, menu):
+    self._entries.append(('m', menu))
+
+  def make(self):
+    print ' menumake', self.label, '##', self._entries
+    menu = tk.Menu(tearoff=False)
+    for entry in self._entries:
+      if len(entry) != 2:
+        raise ValueError, \
+           'Internal error: entry %r has to be two elements' % entry
+      etype, submenu = entry
+      if etype == 'm':  # Another menu
+        menu.add_cascade(label=submenu.label, menu=submenu.make())
+      elif etype == 'a':  # Action
+        menu.add_command(label=submenu.label, command=submenu.action)
+      else:
+        raise ValueError, \
+           'Internal error: entry type %r not recognised' % etype
+    return menu
+
+#    dummy = tk.Menu()
+#    filemenu = tk.Menu(tearoff=False)
+#    filemenu.add_command(label = 'No menu')
+#    dummy.add_cascade(label='File', menu=filemenu)
+#    return dummy
 
 
-class Button(object):
-  def __init__(self, parent, text = ''):
-    self._parent = parent
-    self._text = text
-
-
-class Text(object): pass
-class Table(object): pass
-
+#******************************************************************************
+#
+# Main routine
+#
+#******************************************************************************
 
 if __name__ == '__main__':
 
   # Make a name for the module so we can use the same form as when imported
   ui = sys.modules[__name__]
 
-  # Create a new UI application
-  app = ui.App()
-
-  # Create windows
-  win0 = ui.Master_window(title = 'win0')
-  win1 = ui.Master_window(title = 'win1')
-
-  # Associate windows with the app
-  app.window_add(win0)
-  app.window_add(win1)
-
   # Set up the app's actions
   new_action   = ui.Action('New',   -1, desc = 'Create a new document')
   close_action = ui.Action('Close', -1, desc = 'Close this window')
-  exit_action  = ui.Action('Exit',  -1, desc = 'Quit the application')
+  exit_action  = ui.Action('Exit',  -1, desc = 'Quit the application',
+      action=quit)
   cut_action   = ui.Action('Cut',   -1, desc = 'Cut to clipboard')
   copy_action  = ui.Action('Copy',  -1, desc = 'Copy to clipboard')
   paste_action = ui.Action('Paste', -1, desc = 'Paste from clipboard')
 
   # Create the file menu
-  file_menu = ui.Menu('File', -1)
+  file_menu = ui.Menu('File', -1, desc='File and application controls')
   file_menu.action_add(new_action)
   file_menu.action_add(exit_action)
 
@@ -190,12 +221,16 @@ if __name__ == '__main__':
   window_menu.menu_add(file_menu)
   window_menu.menu_add(edit_menu)
 
-  # Associate the menu with the window
+  # Add the content to a new window
+  win0 = ui.Window(title = 'win0')
   win0.menu_add(window_menu)
 
-  # Make the UI real
-  win0.effect()
+  # Add the window to a new app
+  app = ui.App()
+  app.window_add(win0)
 
-  # Handle user interations
-  response = app.act()
-  print 'App response: %r', response
+  # Make the UI real
+  app.implement()
+
+  # Pass control to the windowing system
+  response = app.interact()
